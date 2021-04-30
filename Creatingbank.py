df54cc5cd49847eeca00bf80ceb6b9e0acf4af38
Felipe Alessander <felipe03114@gmail.com>
2021-04-30 17:01:49 -0300

    Database with python

--- Creatingbank.py
+++ Creatingbank.py
@@ -1,27 +1,34 @@
 import sqlite3
 from sqlite3.dbapi2 import Cursor
-
-
-banco = sqlite3.connect('Dados.db')
-
+print("If you don't want to put the address, the database file will go to the folder where this script is being conducted")
+endereco = input('Location where you want to store the database: ')
+print("Please enter the name of the database because without the name it will not be possible to create it")
+namedate = input('Name of database: ')
+if (endereco != "" and namedate != ""):
+    banco = sqlite3.connect(endereco+'\\'+namedate+'.db')
+elif (namedate != ""):
+    banco = sqlite3.connect(namedate + '.db')
+else:
+    exit()
 
 cursor = banco.cursor()
-exiting= 's'
-while exiting != 'N' or exiting != 'n':
+exiting = 's'
+while (exiting.upper()  != 'N'):
+    but = 's'
     print('Create bank')
     nametable = input('Name of table: ')
     namecolumn = input('Name of colunm: ')
     typecolunm = input('Type of colunm: ')
     cursor.execute("CREATE TABLE '"+nametable+"' ('"+namecolumn+" "+typecolunm+"')")
     cont = input('Do you want to place more columns?(Y or N) ')
-    if cont == "Y" or cont == "y" :
-        while but !='Y' or but != "y":
-             namecolumn = input('Name of colunm: ')
+    if (cont.upper() == "Y"):
+        while (but.upper() != 'N'):
+            namecolumn = input('Name of colunm: ')
             typecolunm = input('Type of colunm: ')
-            cursor.execute("ALTER TABLE '"+nametable+"' ADD '"+namecolumn+" "+typecolunm+"'"
-            but = input("Do you want to continue adding columns?(Y or N) ")
-    banco.commit() # Fechar banco
-    exiting = input("Do you want to continue in the program?(Y or N) ")
+            cursor.execute("ALTER TABLE '"+nametable+"' ADD "+namecolumn+" "+typecolunm)
+            but = input("\nDo you want to continue adding columns?(Y or N) ")
+    exiting = input("\nDo you want to continue in the program?(Y or N) ")
 else:
-    print("Program terminated successfully! ")
+    banco.commit()  # Fechar banco
+    print("\n\nProgram terminated successfully! ")
     print("Thank you for using our program! ")
